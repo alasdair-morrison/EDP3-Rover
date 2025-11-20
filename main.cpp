b651ff53bf087e5f3338b7813941cd4ceab648dd
Alasdair Morrison <2742064m@student.gla.ac.uk>
2025-11-20 14:29:52 +0000

    Fixed Main not saving issue

--- main.cpp
+++ main.cpp
@@ -1,38 +1,16 @@
 #include "mbed.h"
-
-// Pinout Definitions
-PwmOut leftMotor(PTA2);
-DigitalOut leftForwardControl(PTA1);
-DigitalOut leftBackwardControl(PTD4);
-
-PwmOut rightMotor(PTA12);
-DigitalOut rightForwardControl(PTA5);
-DigitalOut rightBackwardControl(PTA4);
-Timer T;
+#include "RoverControl.hpp"
 
 // main() runs in its own thread in the OS
 int main()
 {
-    leftMotor.period(0.01);
-    rightMotor.period(0.01);
     leftForwardControl = 0;
     leftBackwardControl = 0;
     rightForwardControl = 0;
     rightBackwardControl = 0;
-    T.start();
     while (true) {
-        leftMotor = 0.5;
-        rightMotor = 0.5;
-        rightBackwardControl = 1;
-        leftBackwardControl = 1;
-
-        // if ((T.elapsed_time().count() % 10000000) == 0) { //if timer is at a multiple 10s
-        //     rightForwardControl = !rightForwardControl;
-        //     leftForwardControl = !leftForwardControl;
-        //     rightBackwardControl = !rightBackwardControl;
-        //     leftBackwardControl = !leftBackwardControl;
-        // }
-
+        forward(10000000, 0.5);
+        reverse(10000000, 0.5);
     }
 }
 
