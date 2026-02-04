c83bec13645cc836e8daead126d69e7504d47ac1
Alasdair Morrison <alasdair.morrison1002@gmail.com>
2026-02-04 14:08:40 +0000

    added ir sensor control file

--- include/PinOut.hpp
+++ include/PinOut.hpp
@@ -1,11 +1,13 @@
 #include "mbed.h"
-
+//Left Motor Pins
 PwmOut leftMotor(PTA1);
 DigitalOut leftForwardControl(PTA2);
 DigitalOut leftBackwardControl(PTD4);
-
+//Right Motor Pins
 PwmOut rightMotor(PTA12);
 DigitalOut rightForwardControl(PTA5);
 DigitalOut rightBackwardControl(PTA4);
-
+//Detection LED
 PwmOut powerLED(LED_RED);
+
+//IR Sensor Pins
