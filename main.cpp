6a4ab9c4cf026f6985aedcd05e4dfe1654796deb
Alasdair Morrison <2742064m@student.gla.ac.uk>
2026-03-06 12:11:41 +0000

    added small wait statements to end of states

--- main.cpp
+++ main.cpp
@@ -7,6 +7,7 @@ int main()
     stop();
     float period = 1.0/40000;
     float duty = 0.6;
+    int wait_delay = 5000;
     leftMotor.period(period);
     rightMotor.period(period);
 
@@ -20,27 +21,32 @@ int main()
         // If 90 degree right turn is needed
         if (rightTurnValue == 1 && rightValue == 1 && middleValue == 1 && leftValue == 1) {
             cornerRight(duty);
-            
+            wait_us(wait_delay);
         }
         // If 90 degree left turn is needed
         else if (leftTurnValue == 1 && leftValue == 1 && middleValue == 1 && rightValue == 1) {
             cornerLeft(duty);
+            wait_us(wait_delay);
         }
         // If both sensors are on line, move forward
         else if (leftValue == 1 && rightValue == 1 && middleValue == 1) {
             forward(duty);
+            wait_us(wait_delay);
         }
         // Left sensor on black line, turn left
         else if (leftValue == 0 && rightValue == 1 && middleValue == 1) {
             turnLeft(duty);
+            wait_us(wait_delay);
         }
         // Right sensor on black line, turn right
         else if (leftValue == 1 && rightValue == 0 && middleValue == 1) {
             turnRight(duty);
+            wait_us(wait_delay);
         }
         // Both sensors on tile, stop
         else if (leftValue == 0 && rightValue == 0 && middleValue == 0) {
             stop();
+            wait_us(wait_delay);
         }
     }
 }
