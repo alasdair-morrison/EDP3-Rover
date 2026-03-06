2a28da5b988cd620c4171e795c3a0ac6e0a474ce
Alasdair Morrison <2742064m@student.gla.ac.uk>
2026-03-06 10:30:59 +0000

    First draft functional line following

--- main.cpp
+++ main.cpp
@@ -6,42 +6,39 @@ int main()
 {
     stop();
     float period = 1.0/40000;
-    float duty = 0.25;
-    float dutyTurnRight = 0.5;
-    float dutyTurnLeft = 0.5;
-    powerLED.period(2);
+    float duty = 0.6;
     leftMotor.period(period);
     rightMotor.period(period);
 
     while (true) {
-        powerLED.write(0.25);
         int leftValue = leftIR.read();
         int rightValue = rightIR.read();
         int leftTurnValue = leftTurnIR.read();
         int rightTurnValue = rightTurnIR.read();
+        int middleValue = middleIR.read();
 
         // If 90 degree right turn is needed
-        if (leftTurnValue == 1 && rightValue == 1) {
+        if (rightTurnValue == 1 && rightValue == 1 && middleValue == 1 && leftValue == 1) {
             cornerRight(duty);
         }
         // If 90 degree left turn is needed
-        else if (leftTurnValue == 1 && leftValue == 1) {
+        else if (leftTurnValue == 1 && leftValue == 1 && middleValue == 1 && rightValue == 1) {
             cornerLeft(duty);
         }
-        // If both sensors are on white, move forward
-        else if (leftValue == 1 && rightValue == 1) {
+        // If both sensors are on line, move forward
+        else if (leftValue == 1 && rightValue == 1 && middleValue == 1) {
             forward(duty);
         }
         // Left sensor on black line, turn left
-        else if (leftValue == 0 && rightValue == 1) {
+        else if (leftValue == 0 && rightValue == 1 && middleValue == 1) {
             turnLeft(duty);
         }
         // Right sensor on black line, turn right
-        else if (leftValue == 1 && rightValue == 0) {
+        else if (leftValue == 1 && rightValue == 0 && middleValue == 1) {
             turnRight(duty);
         }
-        // Both sensors on black, stop
-        else if (leftValue == 0 && rightValue == 0) {
+        // Both sensors on tile, stop
+        else if (leftValue == 0 && rightValue == 0 && middleValue == 0) {
             stop();
         }
     }
