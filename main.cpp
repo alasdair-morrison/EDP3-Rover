bbb4a8a732c0317f010fd24e738165a193619c38
Alasdair Morrison <2742064m@student.gla.ac.uk>
2026-03-12 16:08:12 +0000

    added handler method for ultrasonic code

--- main.cpp
+++ main.cpp
@@ -19,8 +19,10 @@ DigitalIn rightTurnIR(PTD3);
 DigitalIn leftTurnIR(PTA13);
 
 //Ultrasonic Sensor Pins
-DigitalIn frontTrigger(PTD1);
+DigitalOut frontTrigger(PTD1);
 DigitalIn frontEcho(PTE0);
+DigitalOut sideTrigger();
+DigitalIn sideEcho();
 
 void forward(float duty) {
     leftMotor.write(duty);
@@ -96,6 +98,67 @@ void cornerRight(float dutyTurnLeft) {
     turnRight(dutyTurnLeft);
         wait_us(40000);
 
+}
+
+int FSM = 0;
+Timer High_Time;
+Timer Emergency_Timer;
+double Echo_High_Time = 0.0;
+double Object_Distance = 0.0;
+
+void avoidObstacle() {
+    fullStop();
+    wait_us(5000);
+}
+
+void ultrasonicHandler() {
+    if (FSM == 0 && frontEcho == 0) {
+            frontTrigger = 0;
+            wait_us(1);   // Just a clean transition start
+            frontTrigger = 1;
+            wait_us(10);  // Set Trigger to High for 10 us
+            frontTrigger = 0;
+
+            Emergency_Timer.reset();     // we neeed thhis in case trigger goes of but echo does not due to a loose connection or any reason
+            Emergency_Timer.start();
+
+            FSM = 1;   // Finite-State Machine is on mode 1
+        }
+
+        if (FSM == 1 && frontEcho == 1) {
+            High_Time.reset();
+            High_Time.start();
+
+            Emergency_Timer.stop();     // end the timer since if we get to this point we cant be stuck anymore
+
+            FSM = 2;   // Finite-State Machine is on mode 2
+        }
+
+        if (FSM == 2 && frontEcho == 0) {
+            High_Time.stop();
+            FSM = 3;   // Finite-State Machine is on mode 2
+        }    
+
+        if (FSM == 3 && frontEcho == 0) {
+            // Calculate time in us
+            Echo_High_Time = High_Time.elapsed_time().count();
+            // CONVERTED TO cm/us form m/s from datasheet
+            Object_Distance = (Echo_High_Time * (0.034/ 2.0));
+            FSM = 0;   // Finite-State Machine is on mode 2
+        }    
+
+        float Emergency_Time = Emergency_Timer.elapsed_time().count();
+
+        /*cm and if 100 cm then 5882.35 us time to bounce back*/
+        if (Object_Distance <= 20.0) {
+            avoidObstacle();
+        }
+
+        if ((FSM == 1 || FSM == 2) && Emergency_Time > 60000.0){
+            Emergency_Timer.stop();     // we neeed this in case trigger goes of but echo does not due to a loose connection or any reason
+            Emergency_Timer.reset();
+            FSM = 0; 
+        } // Prevent being stuck if wire is loose and echo nevcer goes high
 }
 
 int main()
